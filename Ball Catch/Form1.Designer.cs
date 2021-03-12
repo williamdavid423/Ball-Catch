acc0986c31d991090248472617f540dd5e2e0e07
billy david <williamdavid423@hotmail.com>
2021-03-12 12:34:13 -0500

    Add project files.

---
+++ Ball Catch/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Ball_Catch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(12, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(174, 23);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(401, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(187, 23);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.timeLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Ball Catch/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ball_Catch
+{
+    public partial class Form1 : Form
+    {
+        int heroX = 280;
+        int heroY = 540;
+        int heroWidth = 40;
+        int heroHeight = 10;
+        int heroSpeed = 10;
+
+        List<int> ballXList = new List<int>();
+        List<int> ballYList = new List<int>();
+        List<int> ballSpeedList = new List<int>();
+        List<string> ballColourList = new List<string>();
+        int ballSize = 10;
+
+        int score = 0;
+        int time = 500;
+
+        bool leftDown = false;
+        bool rightDown = false;
+
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush goldBrush = new SolidBrush(Color.Gold);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //update labels 
+
+            timeLabel.Text = $"Time Left: {time}";
+
+            scoreLabel.Text = $"Score: {score}";
+
+
+
+            //draw ground 
+
+            e.Graphics.FillRectangle(greenBrush, 0, 550, 600, 50);
+
+            //draw hero 
+            e.Graphics.FillRectangle(whiteBrush, heroX, heroY, heroWidth, heroHeight);
+
+            //draw balls 
+            for (int i = 0; i < ballXList.Count(); i++)
+            {
+                if (ballColourList[i] == "red")
+                {
+                    e.Graphics.FillEllipse(redBrush, ballXList[i], ballYList[i], ballSize, ballSize);
+                }
+                else if (ballColourList[i] == "green")
+                {
+                    e.Graphics.FillEllipse(greenBrush, ballXList[i], ballYList[i], ballSize, ballSize);
+                }
+                else if (ballColourList[i] == "gold")
+                {
+                    e.Graphics.FillEllipse(goldBrush, ballXList[i], ballYList[i], ballSize, ballSize);
+                }
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            //move hero 
+            if (leftDown == true && heroX > 0)
+            {
+                heroX -= heroSpeed;
+            }
+
+            if (rightDown == true && heroX < this.Width - heroWidth)
+            {
+                heroX += heroSpeed;
+            }
+
+            //check to see if a new ball should be created 
+            randValue = randGen.Next(0, 101);
+
+            if (randValue < 1) //1% chance of gold ball, (extra time) 
+            {
+                ballXList.Add(randGen.Next(10, this.Width - ballSize * 2));
+                ballYList.Add(10);
+                ballSpeedList.Add(randGen.Next(2, 10));
+                ballColourList.Add("gold");
+            }
+            else if (randValue < 6) //5% change of red ball, (lose points) 
+            {
+                ballXList.Add(randGen.Next(10, this.Width - ballSize * 2));
+                ballYList.Add(10);
+                ballSpeedList.Add(randGen.Next(2, 10));
+                ballColourList.Add("red");
+            }
+            else if (randValue >= 6 && randValue < 11) //5% change of green ball, (get points) 
+            {
+                ballXList.Add(randGen.Next(10, this.Width - ballSize * 2));
+                ballYList.Add(10);
+                ballSpeedList.Add(randGen.Next(2, 10));
+                ballColourList.Add("green");
+            }
+
+            for (int i = 0; i < ballXList.Count(); i++)
+            {
+                ballYList[i] += ballSpeedList[i];
+            }
+
+            //check if ball is below play area and remove it if it is 
+            for (int i = 0; i < ballXList.Count(); i++)
+            {
+                if (ballYList[i] > 550)
+                {
+                    ballXList.RemoveAt(i);
+                    ballYList.RemoveAt(i);
+                    ballSpeedList.RemoveAt(i);
+                    ballColourList.RemoveAt(i);
+                    break;
+                }
+            }
+
+            //check collision of ball and paddle 
+            Rectangle heroRec = new Rectangle(heroX, heroY, heroWidth, heroHeight);
+
+            for (int i = 0; i < ballXList.Count(); i++)
+            {
+                Rectangle ballRec = new Rectangle(ballXList[i], ballYList[i], 10, 10);
+
+                if (heroRec.IntersectsWith(ballRec))
+                {
+                    if (ballColourList[i] == "green")
+                    {
+                        score += 5;
+                    }
+                    else if (ballColourList[i] == "red")
+                    {
+                        score -= 10;
+                    }
+                    else if (ballColourList[i] == "gold")
+                    {
+                        time += 50;
+                    }
+
+                    ballXList.RemoveAt(i);
+                    ballYList.RemoveAt(i);
+                    ballSpeedList.RemoveAt(i);
+                    ballColourList.RemoveAt(i);
+                    break;
+                }
+            }
+
+            //decrease time counter and check to see if time is up 
+            time--;
+
+
+
+            if (time == 0)
+            {
+                gameTimer.Enabled = false;
+            }
+
+       
+              Refresh();
+        }
+    }
+}
